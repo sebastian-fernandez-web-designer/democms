229ba8aad101f3d475ba723042455e6c41668df2
kitdelanzamientodigital <sebastian_6435@hotmail.com>
2025-09-11 18:38:31 +0200

    Update lang.js

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const switchBtn = document.getElementById("lang-switch");
+  const langOptions = document.querySelectorAll(".lang-option");
 
-    switchBtn.addEventListener("click", () => {
-        if (document.body.classList.contains("lang-es")) {
-            document.body.classList.remove("lang-es");
-            document.body.classList.add("lang-en");
-            switchBtn.textContent = "Cambiar a Español";
-        } else {
-            document.body.classList.remove("lang-en");
-            document.body.classList.add("lang-es");
-            switchBtn.textContent = "Cambiar a Inglés";
-        }
+  langOptions.forEach(opt => {
+    opt.addEventListener("click", () => {
+      const lang = opt.dataset.lang;
+
+      if (lang === "en") {
+        document.body.classList.remove("lang-es");
+        document.body.classList.add("lang-en");
+      } else {
+        document.body.classList.remove("lang-en");
+        document.body.classList.add("lang-es");
+      }
     });
+  });
 });
