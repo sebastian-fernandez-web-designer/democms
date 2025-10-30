a46b8d78f2c5b419979435c791675958e7b1572e
kitdelanzamientodigital <sebastian_6435@hotmail.com>
2025-10-30 15:33:24 +0100

    Update loadSection.js

--- assets/js/loadSection.js
+++ assets/js/loadSection.js
@@ -16,7 +16,7 @@
               el.href = data[key];
             } else if (key.includes('_background_color')) { // Nuevo manejo para colores de fondo
               el.style.backgroundColor = data[key];
-            } else if (key.includes('_text_color')) { // <-- NUEVO CÓDIGO AQUÍ
+            } else if (key.includes('_color')) { // <-- NUEVO CÓDIGO AQUÍ
               el.style.color = data[key];
             } else if (el.tagName === "SPAN") {
               el.innerHTML = data[key].trim();
